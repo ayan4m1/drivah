71e73fb8ed7baf24c29d54e90dd011b019adb3d6
Andrew DeLisa <andrew@bulletlogic.com>
2017-09-24 08:53:48 -0400

    flesh out gear display code

--- lib/GearDisplay/GearDisplay.cpp
+++ lib/GearDisplay/GearDisplay.cpp
@@ -1,21 +1,28 @@
 #include "GearDisplay.h"
 
+GearDisplay::GearDisplay() {
+  this->gear = 1;
+  this->init();
+}
+
 void GearDisplay::init() {
-  pinMode(DATA_PIN, OUTPUT);
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(RESET_PIN, OUTPUT);
+
+  this->clear();
 }
 
 void GearDisplay::clear() {
   digitalWrite(LATCH_PIN, LOW);
   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);
   digitalWrite(LATCH_PIN, HIGH);
+  digitalWrite(RESET_PIN, HIGH);
 }
 
-void GearDisplay::update(uint8_t gear) {
-  this->gear = gear;
+void GearDisplay::update() {
   digitalWrite(LATCH_PIN, LOW);
   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, gearCodes[this->gear - 1]);
   digitalWrite(LATCH_PIN, HIGH);
-  delay(300);
 }
--- lib/GearDisplay/GearDisplay.h
+++ lib/GearDisplay/GearDisplay.h
@@ -3,19 +3,25 @@
 #define DATA_PIN  11
 #define LATCH_PIN 12
 #define CLOCK_PIN 13
+#define RESET_PIN 52
 
 // yes, I have a four-speed. look up the others yourself, rich people.
-uint16_t const gearCodes[] = {
+static const uint8_t gearCodes[] = {
   0b11001111, // "1"
   0b10100100, // "2"
   0b10110000, // "3"
-  0b10011001  // "4"
+  0b10011001, // "4"
+  0b11111111  // "0"
 };
 
 class GearDisplay {
-  uint8_t gear;
 public:
+  uint8_t gear;
+
+  GearDisplay();
+  ~GearDisplay();
+
   void init();
   void clear();
-  void update(uint8_t gear);
+  void update();
 };
